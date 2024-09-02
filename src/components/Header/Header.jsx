ed5fb89c6809b1f18631bed3e58c14805d8e22a6
Talibiq123 <talibsaeed90@gmail.com>
2024-09-02 16:58:14 +0530

    Home Page

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './Header.css'
+
+const Header = () => {
+  return (
+    <div className='header'>
+      <div class="header-content">
+        <h2>Order Your Favourite Food Here.</h2>
+        <p>Choose from a Diverse Menu Featuring a Delectable array of Dishes creafted with the Finest Ingredients and Culinary Expertise. Our Mission is to satisfy your Craving and Elevate Your Dining Experience, one Delicous Meal at a Time.</p>
+        <button>View Menu</button>
+      </div>
+    </div>
+  )
+}
+
+export default Header
